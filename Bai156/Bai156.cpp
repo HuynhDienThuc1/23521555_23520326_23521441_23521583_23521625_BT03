ec506371be4ab1b2dfeb8773115319d4df552e21
BuiThiThanhTrang <146336398+BuiThiThanhTrang@users.noreply.github.com>
2023-10-13 15:54:29 +0700

    Trang

--- Bai156/Bai156.cpp
+++ Bai156/Bai156.cpp
@@ -1,19 +1,25 @@
 #include <iostream>
 #include <iomanip>
 using namespace std;
+
 void Nhap(float[], int&);
+void Xuat(float[], int);
 void DaoMang(float[], int);
 void DaoDuong(float[], int);
 
 int main()
 {
-	float a[500];
-	int n;
-	Nhap(a, n);
-	cout << "Mang sau khi dao la: ";
-	DaoMang(a, n);
+	float b[100];
+	int k;
+	Nhap(b, k);
+
+	cout << "\nMang ban dau: ";
+	Xuat(b, k);
+
 	cout << "Cac gia tri cua mang sau sap xep la: ";
-	DaoDuong(a, n);
+	DaoDuong(b, k);
+	Xuat(b, k);
+	cout << "\n\n\nKet thuc!!!";
 	return 0;
 }
 
@@ -21,11 +27,15 @@ void Nhap(float a[], int& n)
 {
 	cout << "Nhap n: ";
 	cin >> n;
-	for (int i = 0; i < n; i++)
-	{
-		cout << "a[" << i << "]:";
-		cin >> a[i];
-	}
+	srand(time(NULL));
+	for (int i = 0; i <= (n - 1); i++)
+		a[i] = -100.0 + (rand() / (RAND_MAX / (100.0 - (-100.0))));
+}
+
+void Xuat(float a[], int n)
+{
+	for (int i = 0; i <= n - 1; i++)
+		cout << fixed << setw(10) << setprecision(3) << a[i];
 }
 
 void DaoMang(float a[], int n)
@@ -46,20 +56,15 @@ void DaoDuong(float a[], int n)
 {
 	float b[100];
 	int k;
-	k = 0;
+	int k = 0;
 	for (int i = 0; i < n; i++)
 		if (a[i] > 0)
 			b[k++] = a[i];
-	DaoMang(b, k);
+
+	DaoMang(a, n);
+
 	k = 0;
 	for (int i = 0; i < n; i++)
 		if (a[i] > 0)
 			a[i] = b[k++];
-	for (int i = 0; i < n; i++)
-		cout << setw(4) << a[i];
-}
-
-void Xuat(int a[], int n)
-{
-
 }

54038910b884bf2979a0af572de790823e39e2ca
MINH-DUNG1 <23520326@gm.uit.edu.vn>
2023-10-13 21:16:07 +0700

    Dũng

--- Bai106/Bai106.cpp
+++ Bai106/Bai106.cpp
@@ -1,52 +1,73 @@
 #include <iostream>
-#include <cmath>
-
+#include <iomanip>
 using namespace std;
 
 void Nhap(int[], int&);
-bool Vitrile(int);
-int Sapxep(int[], int);
+void Xuat(int[], int);
+bool ktDang5m(int);
+int DauTien(int[], int);
+int TimGiaTri(int[], int);
 
 int main()
 {
-	int n;
-	int a[500];
-	Nhap(a, n);
-	if (Sapxep(a, n) == 0)
-		cout << "Mang khong ton tai dang 5m";
-	else
-		cout << "Gia tri lon nhat co dang 5m: " << Sapxep(a, n);
+	int b[100];
+	int k;
+	Nhap(b, k);
+
+	cout << "\nMang ban dau: ";
+	Xuat(b, k);
+
+	cout << "\nGia tri thoa dieu kien: " << TimGiaTri(b, k);
+
+	cout << "\n\n\nKet thuc!!!";
 	return 0;
 }
+
 void Nhap(int a[], int& n)
 {
 	cout << "Nhap n: ";
 	cin >> n;
-	cout << "Nhap mang:\n";
-	for (int i = 0; i < n; i++)
-	{
-		cout << "A[" << i << "] = ";
-		cin >> a[i];
-	}
+	srand(time(NULL));
+	for (int i = 0; i <= n - 1; i++)
+		a[i] = rand() % (200 + 1) - 100;
 }
 
-bool Vitrile(int k)
+void Xuat(int a[], int n)
 {
-	int t = 1;
-	while (t <= k)
-	{
-		if (t == k)
-			return true;
-		t = t * 5;
-	}
+	for (int i = 0; i <= n - 1; i++)
+		cout << setw(10) << a[i];
 }
-int Sapxep(int a[], int n)
-{
-	int ln = 0;
-	for (int i = 0; i < n; i++)
-		if (Vitrile(a[i]) == true)
-			if (ln<a[i])
-				ln = a[i];
-	return ln;
 
+bool ktDang5m(int n)
+{
+	if (n < 1)
+		return false;
+	bool flag = true;
+	int t = abs(n);
+	while (t > 1)
+	{
+		int du = t % 5;
+		if (du != 0)
+			flag = false;
+		t = t / 5;
+	}
+	return flag;
+}
+int DauTien(int a[], int n)
+{
+	for (int i = 0; i < n; i++)
+		if (ktDang5m(a[i]) != 0)
+			return a[i];
+	return 0;
+}
+
+int TimGiaTri(int a[], int n)
+{
+	int lc = DauTien(a, n);
+	if (lc == 0)
+		return 0;
+	for (int i = 0; i < n; i++)
+		if (ktDang5m(a[i]) == 1 && a[i] > lc)
+			lc = a[i];
+	return lc;
 }
